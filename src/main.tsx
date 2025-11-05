6af5e7fd04c00e867d9bb227e9f24f111b31ceb6
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-05 11:53:44 -0500

    correcion main

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from './components/ThemeContext.tsx'
-import './index.css'
+// import './index.css'  // 👈 Comentar esta línea temporalmente
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
