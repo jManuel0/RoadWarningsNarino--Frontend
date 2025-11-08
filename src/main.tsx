97e80e770f828d92a4d0deed768d41ca34ece0a8
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-08 11:16:21 -0500

    modificacion de archivos

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ThemeProvider } from './components/ThemeContext.tsx'
-// import './index.css'  // 👈 Comentar esta línea temporalmente
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -11,3 +11,18 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </ThemeProvider>
   </React.StrictMode>,
 )
+
+
+// Registrar Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((registration) => {
+        console.log('✅ SW registrado:', registration);
+      })
+      .catch((error) => {
+        console.log('❌ SW registro falló:', error);
+      });
+  });
+}
