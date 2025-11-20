a549b77941b94d311329ac514ab7f43facdc5298
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-20 15:18:47 -0500

    fix: Fix Leaflet marker icon display in LocationPickerMap
    - Import Leaflet marker images directly from package
    - Configure default icon paths for Vite compatibility
    - Fixes marker showing as 'Marker' text instead of icon

--- src/components/LocationPickerMap.tsx
+++ src/components/LocationPickerMap.tsx
@@ -2,6 +2,19 @@ import React, { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+// Fix Leaflet default icon issue with Vite
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
+
 interface LocationPickerMapProps {
   lat: number | null;
   lng: number | null;
@@ -40,6 +53,7 @@ const LocationPickerMap: React.FC<LocationPickerMapProps> = ({
         onChange(lat, lng);
       });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
